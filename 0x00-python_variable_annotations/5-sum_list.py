8ae77785dafd5129a6a4bd5161b65d94c0bdadf4
Festongithub <fkemboik@gmail.com>
2024-08-15 10:08:19 +0000

    Type Annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+"""Takes sum of list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """takes list input_list of flaots as arguments
+    and returns their sum as a float
+    """
+    return sum(input_list)
