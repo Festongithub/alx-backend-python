08a9775945cc0746590ff91bfc19433fedc03a91
Festongithub <fkemboik@gmail.com>
2024-08-15 10:43:08 +0000

    Type Annotation

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Functions"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns float multiplier"""
+    def multiplier_function(n: float) -> float:
+        return n * multiplier
+    return multiplier_function
