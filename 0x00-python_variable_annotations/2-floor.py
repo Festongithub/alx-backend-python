602ad8f3c7f9e0413e5bc758fb00fa88fde5d7bc
Festongithub <fkemboik@gmail.com>
2024-08-15 09:46:22 +0000

    Float floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Perform math floor"""
+
+import math
+
+def floor(n: float) -> float:
+    """Takes a float n as argument and returns the floor of the float"""
+    return math.floor(float(n))
