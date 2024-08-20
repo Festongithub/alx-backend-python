2d074d938fb270cf6ccbfd71d1275e359f0e65e0
Festongithub <fkemboik@gmail.com>
2024-08-20 15:12:42 +0000

    Async  generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+"""Async generator"""
+
+
 import asyncio
-import random
+from  random import  uniform
+from typing import Generator
 
-"""The modules tasks on Async generator"""
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yields a rnadom integer"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield uniform(0, 10)
